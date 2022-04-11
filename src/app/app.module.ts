0008f7e2a4d03a548365da988f3e882d16328e8d
Atlegang Molokwane <amolokwane7@gmail.com>
2022-04-11 08:12:45 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,50 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactEditAddComponent } from './contact-edit-add/contact-edit-add.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon'
+import { HttpClientModule } from '@angular/common/http'
+
+
+const routes: Routes =[
+  {
+    path: '',
+    redirectTo: 'contact-list',
+    pathMatch: 'full'
+  },
+{
+  path: 'contact-list',
+  component: ContactListComponent
+},
+{
+  path: 'contact-add',
+component: ContactEditAddComponent
+},
+{
+  path: 'contact-edit/:index',
+component: ContactEditAddComponent
+}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactEditAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule,
+    MatCardModule, 
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
